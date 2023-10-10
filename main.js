abbb54d86ebe888049bc380459e1079b31b03fe9
tenaou <107264846+tenaou@users.noreply.github.com>
2023-10-10 09:35:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 funcion tocasomPom () {
          document.querySelector ('#som_tecla_pom').play ();
-                                 
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla'); 
