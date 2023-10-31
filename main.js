79cfc4bf9ce68fa86ba25016a68b82541a526d7d
tenaou <107264846+tenaou@users.noreply.github.com>
2023-10-31 09:21:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-funcion tocasomPom () {
-         document.querySelector ('#som_tecla_pom').play ();
+function tocasSomPom () {
+document.querySelector('#som_tecla_pom').play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla'); 
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador= 0;
+
+//enquanto
+while (contador < listaDeTeclas.length ){
+
+listaDeTeclas{0}.onclick = tocaSomPom;
+
+contador= contador + 1;
+
+console.log(contador);
+
+         
+}
+
+
